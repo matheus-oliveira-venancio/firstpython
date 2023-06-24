11f1ea9f75e22f67f70cca0d63d5de6bab4983da
Matheus Oliveira <112136868+matheus-oliveira-venancio@users.noreply.github.com>
2023-06-24 00:33:13 -0300

    Add files via upload

---
+++ code.py
@@ -0,0 +1,16 @@
+#hey this is my first code in python
+
+print ("hello world")
+
+# ("this is a string, it always have quotes")
+
+print ('I am Iron Man')
+print ("no, am tony stark")
+print ("no i am soda")
+
+# to make a note just put #
+
+print ('gustavo guanabara aspas e numeros')
+
+print ('i am iron man', 'eu posso escrever . \n ' + 'multi line string')
+# \n will break the line
